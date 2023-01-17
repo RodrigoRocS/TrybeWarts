c4b97061d658fddda889f7e5b2fffde9ee6acab4
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-17 18:15:49 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ const agreement = document.getElementById('agreement');
 const contador = document.getElementById('counter');
 const textarea = document.getElementById('textarea');
 
+const vName = document.getElementById('input-name');
+const vLastName = document.getElementById('input-lastname');
+const vEmail = document.getElementById('input-email');
+const vHouse = document.getElementById('house');
+const vFamily = document.querySelectorAll('input[name="family"]');
+const vSubject = document.querySelectorAll('.subject');
+const vRate = document.querySelectorAll('input[name="rate"]');
+
+const getFamily = () => {
+  let nameFamily = '';
+  for (let i = 0; i < vFamily.length; i += 1) {
+    if (vFamily[i].checked) {
+      nameFamily = vFamily[i].value;
+    }
+  }
+  return nameFamily;
+};
+
+const getSubject = () => {
+  const subjects = [];
+  for (let i = 0; i < vSubject.length; i += 1) {
+    if (vSubject[i].checked) {
+      subjects.push(vSubject[i].value);
+    }
+  }
+  return subjects.join(', ');
+};
+
+const getRate = () => {
+  let rate = '';
+  for (let i = 0; i < vRate.length; i += 1) {
+    if (vRate[i].checked) {
+      rate = vRate[i].value;
+    }
+  }
+  return rate;
+};
+
 btnLogin.addEventListener('click', () => {
   const email = document.getElementById('email');
   const senha = document.getElementById('password');
@@ -24,4 +62,29 @@ agreement.addEventListener('change', () => {
 
 textarea.addEventListener('keyup', () => {
   contador.innerHTML = +(500 - textarea.value.length);
+});
+
+function createDatas(objValue) {
+  const sectionForm = document.querySelector('#form-data');
+
+  for (let index = 0; index < objValue.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = objValue[index];
+    sectionForm.appendChild(span);
+  }
+}
+const form = document.getElementById('evaluation-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const objValue = [
+    `Nome: ${vName.value} ${vLastName.value}`,
+    `Email: ${vEmail.value}`,
+    `Casa: ${vHouse.value}`,
+    `Família: ${getFamily()}`,
+    `Matérias: ${getSubject()}`,
+    `Avaliação: ${getRate()}`,
+    `Observações: ${textarea.value}`,
+  ];
+  createDatas(objValue);
+  form.style.display = 'none';
 });
